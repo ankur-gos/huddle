c92c446bb5f4e725fd82ea5824015e8fe0338ae8
Ankur Goswami <ankur@relianceco.com>
2016-06-21 17:10:31 -0700

    Add stuff

--- huddle/huddleapp/models.py
+++ huddle/huddleapp/models.py
@@ -3,3 +3,8 @@ from __future__ import unicode_literals
 from django.db import models
 
 # Create your models here.
+
+class Post(models.Model):
+    title = models.CharField(max_length=200)
+    body = models.CharField(max_length=500)
+    author = models.CharField(max_length=200)
